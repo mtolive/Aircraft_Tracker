127a6333967168b95883b4e0ea71848edd4edafe
mtolive <mtolive2@asu.edu>
2024-10-29 04:13:35 +0000

    Added Logic

---
+++ include/Filter.h
@@ -0,0 +1,35 @@
+#ifndef FILTER_H
+#define FILTER_H
+
+#include <cstdint>
+#include <string>
+#include <iostream>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <vector>
+#include <tuple>
+
+class Filter{
+
+private:
+
+public:
+    bool filterByDownlinkFormat(uint8_t df, uint8_t expectedDf);
+    bool filterByTypeCode(uint8_t tc, uint8_t expectedTc);
+    bool filterByTypeCode(uint8_t tc, uint8_t minTc, uint8_t maxTc);
+    bool filterByIcao(const std::string& icao, const std::string& expectedIcao);
+    bool filterByCallSign(const std::string& callsign, const std::string& expectedCs);
+    bool filterOddEven(uint8_t oe, uint8_t expectedOe);
+    //std::vector<uint8_t> filterIdMsg(std::vector<uint8_t> message);
+    //std::tuple<std::vector<uint8_t>, std::vector<uint8_t>> filterVelocityMsg(std::vector<uint8_t> message);
+    bool filterIdMsg(uint8_t df, uint8_t tc);
+    bool filterOddMsg(uint8_t df, uint8_t tc, uint8_t oe);
+    bool filterEvenMsg(uint8_t df, uint8_t tc, uint8_t oe);
+    bool filterVelocityMsg(uint8_t df, uint8_t tc);
+
+
+
+};
+
+#endif
---
+++ src/Filter.cpp
@@ -0,0 +1,43 @@
+#include "../include/Filter.h"
+
+bool Filter::filterByDownlinkFormat(uint8_t df, uint8_t expectedDf) {
+    return df == expectedDf;
+}
+
+bool Filter::filterByTypeCode(uint8_t tc, uint8_t expectedTc) {
+    return tc == expectedTc;
+}
+
+bool Filter::filterByTypeCode(uint8_t tc, uint8_t minTc, uint8_t maxTc) {
+    return tc >= minTc && tc <= maxTc;
+}
+
+bool Filter::filterByIcao(const std::string& icao, const std::string& expectedIcao) {
+    return icao.find(expectedIcao) != std::string::npos;
+}
+
+bool Filter::filterByCallSign(const std::string& callsign, const std::string& expectedCs) {
+    return callsign.find(expectedCs) != std::string::npos;
+}
+
+bool Filter::filterOddEven(uint8_t oe, uint8_t expectedOe){
+    return oe == expectedOe;
+}
+
+bool Filter::filterIdMsg(uint8_t df, uint8_t tc){
+    return filterByDownlinkFormat(df, 17) && filterByTypeCode(tc, 1, 4);
+}
+
+bool Filter::filterOddMsg(uint8_t df, uint8_t tc, uint8_t oe){
+    return (filterByDownlinkFormat(df, 17) && filterByTypeCode(tc, 9, 22)
+            && filterOddEven(oe, 1));
+}
+
+bool Filter::filterEvenMsg(uint8_t df, uint8_t tc, uint8_t oe){
+    return (filterByDownlinkFormat(df, 17) && filterByTypeCode(tc, 9, 22)
+            && filterOddEven(oe, 0));
+}
+
+bool Filter::filterVelocityMsg(uint8_t df, uint8_t tc){
+    return (filterByDownlinkFormat(df, 17) && filterByTypeCode(tc, 9, 22));
+}
